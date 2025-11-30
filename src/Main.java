f43e42eab4e2871b9540576aa6efbd4ed3dd1dd4
ruben-encher <ruben.encher@educa.jcyl.es>
2025-11-30 19:38:41 +0100

    add crear entidad cliente

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,25 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Clientes cliente = new Clientes();
+
+        cliente.setNombre(" ");
+        cliente.setApellidos(" ");
+        cliente.setDireccion(" ");
+        cliente.setNIF(" ");
+        cliente.setLocalidad(" ");
+        cliente.setProvincia(" ");
+        cliente.setTelefono(" ");
+        cliente.setEmail(" ");
+
+        System.out.println(cliente.getNombre());
+        System.out.println(cliente.getApellidos());
+        System.out.println(cliente.getDireccion());
+        System.out.println(cliente.getNIF());
+        System.out.println(cliente.getLocalidad());
+        System.out.println(cliente.getProvincia());
+        System.out.println(cliente.getTelefono());
+        System.out.println(cliente.getEmail());
 
     }
 }
