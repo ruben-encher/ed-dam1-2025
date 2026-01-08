b48ed169528bfd5a1bf5102b88f0702c5cb39901
EHR <ruben.encher@educa.jcyl.es>
2026-01-08 19:34:21 +0100

    Add facturas

---
+++ src/Facturas.java
@@ -0,0 +1,48 @@
+public class Facturas {
+
+    private int numeroFactura;
+    private String concepto;
+    private String precio;
+    private String fecha;
+    private String hora;
+
+    public int getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(int numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+}
